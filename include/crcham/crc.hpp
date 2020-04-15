40936461cc4573f483b9a50c447eddf29fbfd793
David Hacker <dmhacker@protonmail.com>
2020-04-15 09:41:11 -0700

    Add table generator for tabular CRC verifier

--- include/crcham/crc.hpp
+++ include/crcham/crc.hpp
@@ -14,11 +14,16 @@ private:
     uint64_t d_generator;
 
 public:
-    __device__ 
+    __device__ __host__
     NaiveCRC(uint64_t koopman);
 
+    __device__ __host__
+    uint64_t polynomial() const;
+    __device__ __host__
+    size_t length() const;
+
     template<size_t N>
-    __device__ 
+    __device__  __host__
     bool verify(Codeword<N> codeword);
 };
 
@@ -30,23 +35,28 @@ private:
     uint64_t d_table[256];
 
 public:
-    __device__ 
+    __device__  __host__
     TabularCRC(uint64_t koopman);
 
+    __device__ __host__
+    uint64_t polynomial() const;
+    __device__ __host__
+    size_t length() const;
+
     template<size_t N>
-    __device__ 
+    __device__  __host__
     bool verify(Codeword<N> codeword);
 };
 
 template<size_t N>
-__device__ 
+__device__ __host__
 bool NaiveCRC::verify(Codeword<N> codeword) {
     // TODO: http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
     return false;
 }
 
 template<size_t N>
-__device__ 
+__device__ __host__
 bool TabularCRC::verify(Codeword<N> codeword) {
     // TODO: http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
     return false;
--- src/crc.cu
+++ src/crc.cu
@@ -1,12 +1,18 @@
 #include <crcham/crc.hpp>
 
+#include <cassert>
+
 namespace crcham {
 
-__device__ 
+__device__ __host__
 NaiveCRC::NaiveCRC(uint64_t koopman)
     : d_generator(koopman)
 {
+#ifdef __CUDA_ARCH__
     d_length = 64 - __clzll(koopman);
+#else
+    d_length = 64 - __builtin_clzll(koopman);
+#endif
     if (d_length > 0) {
         d_generator ^= 1ULL << (d_length - 1);
     }
@@ -14,17 +20,54 @@ NaiveCRC::NaiveCRC(uint64_t koopman)
     d_generator |= 1;
 }
 
- __device__
+__device__ __host__
+uint64_t NaiveCRC::polynomial() const {
+    return d_generator;
+}
+
+__device__ __host__
+size_t NaiveCRC::length() const {
+    return d_length;
+}
+ __device__ __host__
 TabularCRC::TabularCRC(uint64_t koopman) 
     : d_generator(koopman)
 {
+#ifdef __CUDA_ARCH__
     d_length = 64 - __clzll(koopman);
-    if (d_length > 0) {
-        d_generator ^= 1ULL << (d_length - 1);
-    }
+#else
+    d_length = 64 - __builtin_clzll(koopman);
+#endif
+    assert(d_length >= 8);
+    uint64_t mask = 1ULL << (d_length - 1);
+    d_generator ^= mask;
     d_generator <<= 1;
     d_generator |= 1;
-    // TODO: Make table using http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+    for (uint64_t byte = 0; byte < 256; byte++) {
+        uint64_t result = byte << (d_length - 8);
+        for (size_t b = 0; b < 8; b++) {
+            if (result & mask) {
+                result <<= 1;
+                result ^= d_generator;
+            }
+            else {
+                result <<= 1;
+            }
+            result &= (1ULL << d_length) - 1;
+        }
+        d_table[byte] = result;
+    }
 }
+
+__device__ __host__
+uint64_t TabularCRC::polynomial() const {
+    return d_generator;
+}
+
+__device__ __host__
+size_t TabularCRC::length() const {
+    return d_length;
+}
+
 
 }
